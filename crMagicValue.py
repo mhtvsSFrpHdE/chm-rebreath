a0ab5c727d5ad5b7c4f436d83413676ee2ab9c97
mhtvsSFrpHdE <2bemperor47@gmail.com>
2020-03-08 20:26:03 +0800

    Drop old py style config

--- crMagicValue.py
+++ crMagicValue.py
@@ -1,11 +1,3 @@
-configVersion = 0
 
-# chm catalog file extension
-chmCatalogFileExtension = "hhc"
-chmCatalogFileSearchPattern = "*." + chmCatalogFileExtension
 
-chmIndexFileExtension = "hhk"
-chmIFIndexFileSearchPattern = "*." + chmIndexFileExtension
 
-# log file name
-logfileName = "log.txt"
