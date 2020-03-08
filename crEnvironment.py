0dbf58e117bcc30e8906946e398e55d987208eb2
mhtvsSFrpHdE <2bemperor47@gmail.com>
2020-03-08 12:53:47 +0800

    Add environment code base
    - Code to scan and check input folder

---
+++ crEnvironment.py
@@ -0,0 +1,43 @@
+# 3rd
+
+# Environment
+import pathlib  # Path
+
+# Log
+import logging
+
+# My
+import crDevInput  # Information used while development
+import crMagicValue
+import crMessage
+
+from crLog import crPrintCyan
+
+# Scan and get catalog file path
+
+
+def getCatalogFilePath():
+    catalogFilePath = None
+
+    try:
+        catalogFileList = pathlib.Path(crDevInput.unpackedChmFolder).glob(
+            crMagicValue.chmCatalogFileSearchPattern)
+        catalogFileListCount = 0
+
+        for catalogFile in catalogFileList:
+            catalogFileListCount = catalogFileListCount + 1
+
+            if catalogFileListCount > 1:
+                crPrintCyan(crMessage.errMultipleCatalogFile)
+                raise EnvironmentError(crMessage.errMultipleCatalogFile)
+
+            catalogFilePath = catalogFile
+
+        if catalogFileListCount is 0:
+            crPrintCyan(crMessage.errCatalogFileNotFound)
+            raise EnvironmentError(crMessage.errCatalogFileNotFound)
+    except:
+        logging.exception(__name__)
+        raise
+
+    return catalogFilePath
