e29d7acac0d440348c3b5ded3cd884ec50caa435
mhtvsSFrpHdE <2bemperor47@gmail.com>
2020-03-08 12:49:37 +0800

    Add config

---
+++ crMagicValue.py
@@ -0,0 +1,11 @@
+configVersion = 0
+
+# chm catalog file extension
+chmCatalogFileExtension = "hhc"
+chmCatalogFileSearchPattern = "*." + chmCatalogFileExtension
+
+chmIndexFileExtension = "hhk"
+chmIFIndexFileSearchPattern = "*." + chmIndexFileExtension
+
+# log file name
+logfileName = "log.txt"
---
+++ crMessage.py
@@ -0,0 +1,5 @@
+configVersion = 0
+
+# Error message
+errMultipleCatalogFile = "More than one catalog file in folder, check why."
+errCatalogFileNotFound = "Catalog file not found, check why."
