1ef36c8425baee5003bed97c4e59073a5503bf52
mhtvsSFrpHdE <2bemperor47@gmail.com>
2020-03-08 13:00:02 +0800

    A demo to run exist code
    Try to print chm catalog file path, may raise error and get logged.

---
+++ chm-rebreath.py
@@ -0,0 +1,24 @@
+# 3rd
+import pathlib  # Path
+import logging
+
+from bs4 import BeautifulSoup  # HTML parsing
+
+# My
+import crMagicValue
+
+from crEnvironmentHeader import *
+
+# Initialize log file
+# We do not dealing with external error, just print and crash
+logging.basicConfig(filename=crMagicValue.logfileName)
+
+# int main () {
+
+
+def main():
+    print(getCatalogFilePath())
+
+
+#}
+main()
