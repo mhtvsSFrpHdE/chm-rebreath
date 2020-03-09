8b300a2cb627de670c56504c68f280e803953458
mhtvsSFrpHdE <2bemperor47@gmail.com>
2020-03-09 17:25:07 +0800

    Add new error message

--- crMessage.py
+++ crMessage.py
@@ -1,10 +1,16 @@
 def _my_message_preprocessor(message_config):
     mErr = message_config['err']
 
+    # TODO: use a loop to replace all the symbols
+
     # .ini config doesn't support space at end of the line,
     # they will cap by three double quotes
     message_config['err']['software_broken'] = mErr['software_broken'].strip(
         '"""')
+
+    # %nl% is next line, refer to \n
+    message_config['err']['chm_catalog_multiple_sub_node_ul'] = mErr['chm_catalog_multiple_sub_node_ul'].strip(
+        '"""').replace("<nl>", "\n")
 
     return message_config
 
