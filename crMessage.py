6798521abfdb0d56abde4b2853e95a8124652773
mhtvsSFrpHdE <2bemperor47@gmail.com>
2020-03-11 22:28:03 +0800

    Update comment

--- crMessage.py
+++ crMessage.py
@@ -1,8 +1,6 @@
 def _preprocess_message_config(message_config):
     mErr = message_config['err']
 
-    # TODO: use a loop to replace all the symbols
-
     # .ini config doesn't support space at end of the line,
     # they will cap by three double quotes
     message_config['err']['software_broken'] = mErr['software_broken'].strip(
@@ -15,6 +13,12 @@ def _preprocess_message_config(message_config):
     return message_config
 
 # Some message entry need to preprocess before use
+#
+# TODO: use a loop to replace all the symbols
+#
+# Update: too bad, failed to use loop to update the values
+# It seems configparser doesn't support iterate through it and update value
+# Perhaps a stackoverflow can help
 
 
 def get_preprocessed_message(message_config):
