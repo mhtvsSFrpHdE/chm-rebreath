f9bc5a02028ec02c2c405cd1fa7854ac40decf6d
mhtvsSFrpHdE <2bemperor47@gmail.com>
2020-03-11 12:47:19 +0800

    Add bootstrap

--- chm-rebreath.py
+++ chm-rebreath.py
@@ -1,20 +1,8 @@
 # 3rd
-import logging  # NOQA: E402
-#
-# Prepare logging at the very beginning
-# We do not dealing with external error, just print and crash
-logging.basicConfig(filename="log.txt")  # NOQA: E402
-
 from bs4 import BeautifulSoup  # HTML parsing
 
 # My
-from crConfigHeader import *
-from crEnvironmentHeader import *
-from crCoreHeader import *
-
-# Initialize
-init_environment(message_config, magic_value_config)
-init_core_get_catalog(message_config)
+from crBootstrap import *
 
 # int main () {
 
---
+++ crBootstrap.py
@@ -0,0 +1,15 @@
+# 3rd
+import logging  # NOQA: E402
+#
+# Prepare logging at the very beginning
+# We do not dealing with external error, just print and crash
+logging.basicConfig(filename="log.txt")  # NOQA: E402
+
+# My
+from crConfigHeader import *
+from crEnvironmentHeader import *
+from crCoreHeader import *
+
+# Initialize
+init_environment(message_config, magic_value_config)
+init_core_get_catalog(message_config)
