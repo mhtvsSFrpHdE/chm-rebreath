0754e3baa54a4abe19b7be7e5095ef153ba71065
mhtvsSFrpHdE <2bemperor47@gmail.com>
2020-03-25 10:41:49 +0800

    Short function call

--- chm-rebreath.py
+++ chm-rebreath.py
@@ -16,7 +16,8 @@ def main():
         # TODO: Test purpose code, move to other place later
         catalog_html_text = get_catalog_html_text(catalog_node)
         output_folder_path = get_root_output_folder_path()
-        with open(get_catalog_html_output_path(catalog_node.catalog_name), "w+", encoding="utf-8") as catalog_html_file:
+        catalog_html_output_path = get_catalog_html_output_path(catalog_node.catalog_name)
+        with open(catalog_html_output_path, "w+", encoding="utf-8") as catalog_html_file:
             catalog_html_file.write(catalog_html_text)
             copy_catalog_html_resource(output_folder_path)
 
