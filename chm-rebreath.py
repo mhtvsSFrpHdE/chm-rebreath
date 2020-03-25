6eb09e8f830e2a15a348119fae2aaa01e7d89ee3
mhtvsSFrpHdE <2bemperor47@gmail.com>
2020-03-25 10:42:19 +0800

    Code format

--- chm-rebreath.py
+++ chm-rebreath.py
@@ -17,12 +17,15 @@ def main():
         catalog_html_text = get_catalog_html_text(catalog_node)
         output_folder_path = get_root_output_folder_path()
         catalog_html_output_path = get_catalog_html_output_path(catalog_node.catalog_name)
+
         with open(catalog_html_output_path, "w+", encoding="utf-8") as catalog_html_file:
             catalog_html_file.write(catalog_html_text)
 
         copy_catalog_html_resource(output_folder_path)
 
-#}
+
+# }
+
 main()
 
 logging.info("#========= End properly after main() =#")  # NOQA: E402
