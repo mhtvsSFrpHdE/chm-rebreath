9e3d7c9f93c9253911e08481fc92c41110fcb883
mhtvsSFrpHdE <2bemperor47@gmail.com>
2020-03-27 13:31:00 +0800

    Code format

--- crEnvironmentHeader.py
+++ crEnvironmentHeader.py
@@ -1,8 +1,11 @@
 # Need to know where the required file stored in the computer,
 # an object that can affect by environment belongs here
 from crEnvironment import confirm_folder_exist
+
 from crEnvironment import init_environment
 from crEnvironment import get_preprocessed_environment
-from crEnvironment import get_root_output_folder_path
 from crEnvironment import get_catalog_chm_file_path
-from crEnvironment import get_catalog_html_output_path
+
+from crEnvironment import get_root_output_folder_full_path
+from crEnvironment import get_catalog_html_output_full_path
+from crEnvironment import get_catalog_html_resource_output_full_path
