35d30297dd31f7e7b1306ec153df7742c253aacf
mhtvsSFrpHdE <2bemperor47@gmail.com>
2020-03-27 15:56:37 +0800

    Track rename affect

--- chm-rebreath.py
+++ chm-rebreath.py
@@ -14,8 +14,8 @@ def main():
     # Catalog node placeholder
     catalog_node = None
 
-    with open(get_catalog_chm_file_path(), "rb") as chm_catalog_file:
     # Open 
+    with open(get_catalog_chm_file_full_path(), "rb") as chm_catalog_file:
         mySoup = BeautifulSoup(chm_catalog_file, "html5lib")
         catalog_node = get_catalog_node(mySoup)
 
