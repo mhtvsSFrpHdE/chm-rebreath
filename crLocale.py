c3caeeba36821ab9d6ead76a11355a0b2636bb46
mhtvsSFrpHdE <2bemperor47@gmail.com>
2020-03-27 12:32:57 +0800

    Rename variable

--- crLocale.py
+++ crLocale.py
@@ -17,11 +17,11 @@ def _my_locale_parser(config_environment):
     # Check system language
     language_root = config_environment['language']['root_path']
     system_language = get_system_language()
-    language_current = language_root + system_language + '/'  # language/en_US/
+    current_language = language_root + system_language + '/'  # language/en_US/
 
-    current_language_exists = osPath.exists(plPath(language_current))
     if current_language_exists is True:
-        language_current = config_environment['language']['fallback']
+    current_language_exists = osPath.exists(plPath(current_language))
+        current_language = config_environment['language']['fallback_path']
     #
 
     # Apply language to path
@@ -30,7 +30,7 @@ def _my_locale_parser(config_environment):
     # Get
     config_message_path = config_environment['message']['message_config_path']
     # Apply
-    config_message_path = language_current + config_message_path  # language/en_US/message.xml
+    config_message_path = current_language + config_message_path  # language/en_US/message.xml
 
     # Put it back
     config_environment['message']['config_message_path'] = config_message_path
