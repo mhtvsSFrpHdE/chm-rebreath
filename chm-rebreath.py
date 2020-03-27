58dfeb72d44e4a91eb32c663ed3fab2c8825f8f8
mhtvsSFrpHdE <2bemperor47@gmail.com>
2020-03-27 13:41:21 +0800

    Move some code out the open scope

--- chm-rebreath.py
+++ chm-rebreath.py
@@ -11,14 +11,16 @@ from crLogHeader import *
 
 
 def main():
+    catalog_node = None
+
     with open(get_catalog_chm_file_path(), "rb") as chm_catalog_file:
         mySoup = BeautifulSoup(chm_catalog_file, "html5lib")
         catalog_node = get_catalog_node(mySoup)
 
-        # TODO: Test purpose code, move to other place later
-        catalog_html_text = get_catalog_html_text(catalog_node)
-        output_folder_path = get_root_output_folder_path()
-        catalog_html_output_path = get_catalog_html_output_path(catalog_node.catalog_name)
+    # TODO: Test purpose code, move to other place later
+    catalog_html_text = get_catalog_html_text(catalog_node)
+    root_output_folder_full_path = get_root_output_folder_full_path()
+    catalog_html_resource_output_full_path = get_catalog_html_resource_output_full_path(root_output_folder_full_path)
 
         with open(catalog_html_output_path, "w+", encoding="utf-8") as catalog_html_file:
             catalog_html_file.write(catalog_html_text)
