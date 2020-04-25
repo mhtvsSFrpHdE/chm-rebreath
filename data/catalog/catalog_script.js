ecbfcc97eb20f35337c461e658ab8fbb74e40ba1
mhtvsSFrpHdE <2bemperor47@gmail.com>
2020-04-26 02:40:12 +0800

    Handle html onload event

--- data/catalog/catalog_script.js
+++ data/catalog/catalog_script.js
@@ -1,4 +1,17 @@
 // https://www.w3schools.com/howto/howto_js_treeview.asp
 
   }
+/** Initialize catalog div */
+function initCatalogDiv() {
+  // Find and expand root level catalog node list
+  // They are all folded by default
+  let rootCatalogNode = document.querySelector('.catalog_node_list.root');
+  let rootCatalogNodeList = getTopLevelCatalogNodeList(rootCatalogNode);
+
+  toggleCatalogNodeListExpandStatus(rootCatalogNodeList);
+}
+
+/** When html body have loaded */
+function onloadHtmlBody() {
+  initCatalogDiv();
 }
