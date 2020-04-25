048257a8339b296a579d3425304b18cb6f612010
mhtvsSFrpHdE <2bemperor47@gmail.com>
2020-04-26 02:21:04 +0800

    Remove code example

--- data/catalog/catalog_script.js
+++ data/catalog/catalog_script.js
@@ -1,13 +1,4 @@
 // https://www.w3schools.com/howto/howto_js_treeview.asp
 
-function catalogOnLoad() {
-  var toggler = document.getElementsByClassName("caret");
-  var i;
-
-  for (i = 0; i < toggler.length; i++) {
-    toggler[i].addEventListener("click", function () {
-      this.parentElement.querySelector(".nested").classList.toggle("active");
-      this.classList.toggle("caret-down");
-    });
   }
 }
