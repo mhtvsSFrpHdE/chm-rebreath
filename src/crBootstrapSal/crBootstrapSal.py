cbaa8251d02959f639eb1ea48a8c99988c8ab9f2
mhtvsSFrpHdE <2bemperor47@gmail.com>
2020-06-13 01:01:46 +0800

    Fix modules receiving raw config

--- src/crBootstrapSal/crBootstrapSal.py
+++ src/crBootstrapSal/crBootstrapSal.py
@@ -7,15 +7,17 @@ import crOutputHeader as crOutput  # NOQA: E402
 
 
 def init_bootstrap_sal(environment_config, magic_value_config, message_config):
-    crEnvironmentSal.init_environment_sal(magic_value_config, message_config)
-
-    crCore.init_core_get_catalog_node(environment_config, message_config)
-    crCore.init_core_get_index_html(environment_config, message_config)
-
-    crOutput.init_output(environment_config, message_config)
-
     # Create modified config
     myConfigGroup = environment_config, magic_value_config, message_config
     environment_config_local, magic_value_config_local, message_config_local = _crConfigSal.init_config_sal(myConfigGroup, crEnvironmentSal)
 
+    # Use modified config to initialize other module
+    crEnvironmentSal.init_environment_sal(magic_value_config_local, message_config_local)
+
+    crCore.init_core_get_catalog_node(environment_config_local, message_config_local)
+    crCore.init_core_get_index_html(environment_config_local, message_config_local)
+
+    crOutput.init_output(environment_config_local, message_config_local)
+
+    # Transfer modified config back to main level scope
     return environment_config_local, magic_value_config_local, message_config_local
