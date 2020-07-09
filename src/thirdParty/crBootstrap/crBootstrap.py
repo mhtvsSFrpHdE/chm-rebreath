05554c3772efa95a2c495ecedde4bdfcbe15ae11
mhtvsSFrpHdE <2bemperor47@gmail.com>
2020-07-09 21:54:50 +0800

    Trim bootstrap logic
    Remove environment utils for a while to rethink how to code them

--- src/thirdParty/crBootstrap/crBootstrap.py
+++ src/thirdParty/crBootstrap/crBootstrap.py
@@ -1,6 +1,5 @@
-# Prepare everything about environment, logging, config, international
+# Prepare everything about logging, config, international
 # Result in one line `from crBootstrap import *` in the main method or file
-
 
 # Prepare logging at the very beginning
 # We do not dealing with external error, just print and crash
@@ -9,15 +8,8 @@ import logging  # NOQA: E402
 logging.basicConfig(filename="log.txt", level=logging.INFO)  # NOQA: E402
 logging.info("#========= Start =========#")  # NOQA: E402
 
+# Load config
+from crConfig import *  # NOQA: E402
+
 # Prepare command line arguments at the very beginning
 from crArgumentParser import *
-
-
-# Other modules
-from crConfig import *  # NOQA: E402
-import crEnvironmentHeader as crEnvironment  # NOQA: E402
-
-
-# Some module require config file to initialize,
-# call the init method here
-crEnvironment.init_environment(message_config)
