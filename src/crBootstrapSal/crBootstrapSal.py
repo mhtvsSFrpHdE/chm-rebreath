0ca5628904158e5d0a3c649b203244eda5ca003e
mhtvsSFrpHdE <2bemperor47@gmail.com>
2020-07-24 10:12:54 +0800

    Trim not required public import

--- src/crBootstrapSal/crBootstrapSal.py
+++ src/crBootstrapSal/crBootstrapSal.py
@@ -1,7 +1,7 @@
 # Load project modules
-import crConfigSal as _crConfigSal  # NOQA: E402
+import crConfigSal as _crConfig  # NOQA: E402
 import crCore  # NOQA: E402
-import crEnvironmentSal  # NOQA: E402
+import crEnvironmentSal as crEnvironment  # NOQA: E402
 import crOutput  # NOQA: E402
 import crUnpack  # NOQA: E402
 
@@ -18,14 +18,14 @@ def _apply_config_preprocessor():
     global magic_value_config
     global message_config
 
-    environment_config, magic_value_config, message_config = _crConfigSal.apply_config_preprocessor_sal(environment_config, magic_value_config, message_config)
+    environment_config, magic_value_config, message_config = _crConfig.apply_config_preprocessor_sal(environment_config, magic_value_config, message_config)
 
 # The method will use preprocessed config to initialize other module
 
 
 def _init_any_other_module_requires_config():
     # Init basic module
-    crEnvironmentSal.init_environment_sal(magic_value_config, message_config)
+    crEnvironment.init_environment_sal(magic_value_config, message_config)
 
     # crCore
     crCore.init_core_get_catalog_node(environment_config, message_config)
